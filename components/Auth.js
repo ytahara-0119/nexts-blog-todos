5251bc87e1abd35a366f60ed2eb5f742f0b0b678
田原芳信 <taharayoshinobu@MacBook-Air.local>
2021-09-24 09:17:41 +0900

    add Login logic

--- components/Auth.js
+++ components/Auth.js
@@ -1,25 +1,109 @@
+import {useRouter} from "next/router";
+import {useState} from "react";
+import Cookie from "universal-cookie";
+
+const cookie = new Cookie();
+
 export default function Auth() {
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
+
+  const login = async () => {
+    try {
+      await fetch(
+        `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/auth/jwt/create/`,
+        {
+          method: "POST",
+          body: JSON.stringify({ username: username, password: password}),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        if (res.status === 400) {
+          throw "authentication failed";
+        } else if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        const options = { path: "/" };
+        cookie.set("access_token", data.access, options);
+      });
+      router.push("/main-page");
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const authUser = async (e) => {
+    e.preventDefault();
+    if (isLogin) {
+      login();
+    } else {
+      try {
+        await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/register/`, {
+          method: "POST",
+          body: JSON.stringify({ username: username, password: password }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then((res) => {
+          if (res.status === 400) {
+            throw "authentication failed";
+          }
+        });
+        login();
+      } catch (err) {
+        alert(err);
+      }
+    }
+  };
+
   return   <div className="max-w-md w-full space-y-8">
     <div>
       <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow"/>
       <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
-        Sign in to your account
+        {isLogin ? "Login" : "Sign up"}
       </h2>
     </div>
-    <form className="mt-8 space-y-6" action="#" method="POST">
+    <form className="mt-8 space-y-6" onSubmit={authUser}>
       <input type="hidden" name="remember" value="true"/>
       <div className="rounded-md shadow-sm -space-y-px">
         <div>
-          <input id="email-address" name="email" type="email" autoComplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address"/>
+          <input 
+            name="username"
+            type="text"
+            autoComplete="username"
+            required
+            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
+            placeholder="Username"/>
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
         </div>
         <div>
-          <input id="password" name="password" type="password" autoComplete="current-password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password"/>
+          <input 
+            name="password" 
+            type="password" 
+            autoComplete="current-password" 
+            required 
+            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
+            placeholder="Password"/>
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
         </div>
       </div>
 
       <div className="flex items-center justify-center">
         <div className="text-sm">
-          <span className="cursor-pointer font-medium text-white hover:text-indigo-500">
+          <span onClick={() => setIsLogin(!isLogin)} className="cursor-pointer font-medium text-white hover:text-indigo-500">
             change mode ?
           </span>
         </div>
@@ -32,7 +116,7 @@ export default function Auth() {
               <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
             </svg>
           </span>
-          Sign in
+          {isLogin ? "Login with JWT" : "Create new user"}
         </button>
       </div>
     </form>

3c6e3b9415d8910eebb0231fbfebe9450df15368
田原芳信 <taharayoshinobu@MacBook-Air.local>
2021-09-25 15:06:11 +0900

    add revalidate settings

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -36,5 +36,6 @@ export async function getStaticProps() {
   const filteredPosts = await getAllPostsData();
   return {
     props: { filteredPosts },
+    revalidate: 3,
   };
 }
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -54,5 +54,6 @@ export async function getStaticProps({ params }) {
     props: {
       post,
     },
+    revalidate: 3,
   };
 }

54da9ae0374e4932d128fad69cac03c46cc9e422
田原芳信 <taharayoshinobu@MacBook-Air.local>
2021-09-26 20:38:06 +0900

    add create and update

--- components/Task.js
+++ components/Task.js
@@ -1,9 +1,13 @@
 import Link from "next/link";
+import { useContext } from "react";
 import Cookie from "universal-cookie";
+import { StateContext } from "../context/StateContext";
 
 const cookie = new Cookie();
 
 export default function Task({ task, taskDeleted }) {
+
+  const { setSeletedTask } = useContext(StateContext);
   
   const deleteTask = async () => {
     await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/tasks/${task.id}`, {
@@ -30,6 +34,20 @@ export default function Task({ task, taskDeleted }) {
         </span>
       </Link>
       <div className="float-right ml-20">
+        <svg 
+          onClick={() => setSeletedTask(task)}
+          className="w-6 h-6 float-left cursor-pointer" 
+          fill="none" 
+          stroke="currentColor" 
+          viewBox="0 0 24 24" 
+          xmlns="http://www.w3.org/2000/svg">
+          <path 
+            strokeLinecap="round" 
+            strokeLinejoin="round" 
+            strokeWidth="2" 
+            d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
+          </path>
+        </svg>
         <svg 
           onClick={deleteTask}
           className="w-6 h-6 mr-2 cursor-pointer" 
--- pages/task-page.js
+++ pages/task-page.js
@@ -3,6 +3,8 @@ import { useEffect } from "react";
 import useSWR from "swr";
 import Layout from "../components/Layout";
 import Task from "../components/Task";
+import TaskFrom from "../components/TaskForm";
+import StateContextProvider from "../context/StateContext";
 import { getAllTasksData } from "../lib/tasks";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
@@ -21,30 +23,33 @@ export default function TaskPage({ staticfilterdTasks }) {
   }, []);
 
   return (
-    <Layout title="Task page">
-      <ul>
-        {filteredTasks && 
-           filteredTasks.map((task) => <Task key={task.id} task={task} taskDeleted={mutate} />)}
-      </ul>
-      <Link href="/main-page">
-        <div className="flex cursor-pointer mt-12">
-          <svg 
-            className="w-6 h-6" 
-            fill="none" 
-            stroke="currentColor" 
-            viewBox="0 0 24 24" 
-            xmlns="http://www.w3.org/2000/svg">
-            <path 
-              strokeLinecap="round" 
-              strokeLinejoin="round" 
-              strokeWidth="2" 
-              d="M11 19l-7-7 7-7m8 14l-7-7 7-7">
-            </path>
-          </svg>
-          <span>Back to main page</span>
-        </div>
-      </Link>
-    </Layout>
+    <StateContextProvider>
+      <Layout title="Task page">
+      <TaskFrom taskCreated={mutate} />
+        <ul>
+          {filteredTasks && 
+             filteredTasks.map((task) => <Task key={task.id} task={task} taskDeleted={mutate} />)}
+        </ul>
+        <Link href="/main-page">
+          <div className="flex cursor-pointer mt-12">
+            <svg 
+              className="w-6 h-6" 
+              fill="none" 
+              stroke="currentColor" 
+              viewBox="0 0 24 24" 
+              xmlns="http://www.w3.org/2000/svg">
+              <path 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                strokeWidth="2" 
+                d="M11 19l-7-7 7-7m8 14l-7-7 7-7">
+              </path>
+            </svg>
+            <span>Back to main page</span>
+          </div>
+        </Link>
+      </Layout>
+    </StateContextProvider>
   );
 }
 export async function getStaticProps() {
